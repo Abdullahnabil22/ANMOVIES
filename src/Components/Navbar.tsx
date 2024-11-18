2cb03f4c43dd2941f609c366371000d158e42c97
Abdullahnabil22 <abdullahn.work@gmail.com>
2024-11-18 20:00:13 +0200

    responsive update

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -25,7 +25,6 @@ function NavBar({ isSeries }: { isSeries: boolean }) {
   const t = useTranslations("Home");
   const locale = useLocale();
   const isRTL = locale === "ar";
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -48,10 +47,10 @@ function NavBar({ isSeries }: { isSeries: boolean }) {
   }
 
   return (
-    <nav className="bg-transparent backdrop-blur-sm sticky top-0 z-50 navbar border-b border-white/5 ">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="bg-transparent backdrop-blur-sm sticky top-0 z-50 navbar border-b border-white/5">
+      <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
         <div
-          className={`flex items-center justify-between h-20 ${
+          className={`flex items-center justify-between h-16 sm:h-20 ${
             isRTL ? "flex-row-reverse" : ""
           }`}
         >
@@ -61,12 +60,12 @@ function NavBar({ isSeries }: { isSeries: boolean }) {
               isRTL ? "space-x-reverse" : "space-x-2"
             } group`}
           >
-            <span className="font-['Audiowide'] text-4xl font-bold">
+            <span className="font-['Audiowide'] text-2xl sm:text-3xl md:text-4xl font-bold">
               <span
                 className={
                   isSeries
                     ? "blue-gradient"
-                    : "text-transparent bg-gradient-to-r from-red-500 via-red-600 to-red-700 bg-clip-text "
+                    : "text-transparent bg-gradient-to-r from-red-500 via-red-600 to-red-700 bg-clip-text"
                 }
               >
                 AN
@@ -77,55 +76,64 @@ function NavBar({ isSeries }: { isSeries: boolean }) {
             </span>
           </Link>
 
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="block md:hidden mobile-menu-button text-white p-2 hover:bg-white/10 rounded-lg transition-all duration-200"
-          >
-            {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-          </button>
-
           <div
-            className={`hidden md:flex items-center gap-1 ${
+            className={`flex items-center gap-0.5 sm:gap-1 ${
               isRTL ? "flex-row-reverse" : ""
             }`}
           >
             <Link
               href="/movies"
-              className={`flex items-center gap-2 ${
+              className={`flex items-center gap-1 sm:gap-2 ${
                 isRTL ? "flex-row-reverse" : ""
-              } px-4 py-2 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-all duration-200`}
+              } 
+                px-2 sm:px-3 md:px-4 py-1.5 sm:py-2 rounded-lg text-gray-300 hover:text-white 
+                hover:bg-white/10 transition-all duration-200`}
             >
-              <FaFilm className={isSeries ? "text-blue-500" : "text-red-500"} />
-              <span>{t("Movies")}</span>
+              <FaFilm
+                className={`text-sm sm:text-base ${
+                  isSeries ? "text-blue-500" : "text-red-500"
+                }`}
+              />
+              <span className="hidden md:inline">{t("Movies")}</span>
             </Link>
 
             <Link
               href="/series"
-              className={`flex items-center gap-2 ${
+              className={`flex items-center gap-1 sm:gap-2 ${
                 isRTL ? "flex-row-reverse" : ""
-              } px-4 py-2 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-all duration-200`}
+              } 
+                px-2 sm:px-3 md:px-4 py-1.5 sm:py-2 rounded-lg text-gray-300 hover:text-white 
+                hover:bg-white/10 transition-all duration-200`}
             >
-              <FaTv className={isSeries ? "text-blue-500" : "text-red-500"} />
-              <span>{t("Series")}</span>
+              <FaTv
+                className={`text-sm sm:text-base ${
+                  isSeries ? "text-blue-500" : "text-red-500"
+                }`}
+              />
+              <span className="hidden md:inline">{t("Series")}</span>
             </Link>
 
             {isLogged && (
               <Link
                 href="/favorites"
-                className={`flex items-center gap-2 ${
+                className={`flex items-center gap-1 sm:gap-2 ${
                   isRTL ? "flex-row-reverse" : ""
-                } px-4 py-2 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-all duration-200`}
+                } 
+                  px-2 sm:px-3 md:px-4 py-1.5 sm:py-2 rounded-lg text-gray-300 hover:text-white 
+                  hover:bg-white/10 transition-all duration-200`}
               >
                 <FaHeart
-                  className={isSeries ? "text-blue-500" : "text-red-500"}
+                  className={`text-sm sm:text-base ${
+                    isSeries ? "text-blue-500" : "text-red-500"
+                  }`}
                 />
-                <span>{t("Favorites")}</span>
+                <span className="hidden md:inline">{t("Favorites")}</span>
               </Link>
             )}
           </div>
 
           <div
-            className={`hidden lg:flex items-center gap-4 ${
+            className={`flex items-center gap-2 sm:gap-4 ${
               isRTL ? "flex-row-reverse" : ""
             }`}
           >
@@ -135,52 +143,60 @@ function NavBar({ isSeries }: { isSeries: boolean }) {
                   localStorage.removeItem("token");
                   setIsLogged(false);
                 }}
-                className={`flex items-center gap-2 ${
+                className={`flex items-center gap-1 sm:gap-2 ${
                   isRTL ? "flex-row-reverse" : ""
-                } px-4 py-2 ${
-                  isSeries
-                    ? "bg-blue-600 hover:bg-blue-700"
-                    : "bg-red-600 hover:bg-red-700"
-                } text-white rounded-lg transition-all duration-200 text-sm font-medium`}
+                } 
+                  px-2 sm:px-3 md:px-4 py-1.5 sm:py-2 
+                  ${
+                    isSeries
+                      ? "bg-blue-600 hover:bg-blue-700"
+                      : "bg-red-600 hover:bg-red-700"
+                  }
+                  text-white rounded-lg transition-all duration-200 text-xs sm:text-sm font-medium`}
               >
-                <FaSignOutAlt className="text-sm" />
-                <span>{t("Logout")}</span>
+                <FaSignOutAlt className="text-xs sm:text-sm" />
+                <span className="hidden md:inline">{t("Logout")}</span>
               </button>
             ) : (
               <div
-                className={`flex items-center gap-3 ${
+                className={`flex items-center gap-1 sm:gap-3 ${
                   isRTL ? "flex-row-reverse" : ""
                 }`}
               >
                 <Link
                   href="/login"
-                  className={`flex items-center gap-2 ${
+                  className={`flex items-center gap-1 sm:gap-2 ${
                     isRTL ? "flex-row-reverse" : ""
-                  } px-4 py-2 text-gray-300 hover:text-white hover:bg-white/10 rounded-lg transition-all duration-200 text-sm font-medium`}
+                  } 
+                    px-2 sm:px-3 md:px-4 py-1.5 sm:py-2 text-gray-300 hover:text-white 
+                    hover:bg-white/10 rounded-lg transition-all duration-200 text-xs sm:text-sm font-medium`}
                 >
-                  <FaSignInAlt className="text-sm" />
-                  <span>{t("Login")}</span>
+                  <FaSignInAlt className="text-xs sm:text-sm" />
+                  <span className="hidden md:inline">{t("Login")}</span>
                 </Link>
                 <Link
                   href="/register"
-                  className={`flex items-center gap-2 ${
+                  className={`flex items-center gap-1 sm:gap-2 ${
                     isRTL ? "flex-row-reverse" : ""
-                  } px-4 py-2 ${
-                    isSeries
-                      ? "bg-blue-500 hover:bg-blue-600"
-                      : "bg-red-500 hover:bg-red-600"
-                  } text-white rounded-lg transition-all duration-200 text-sm font-medium`}
+                  } 
+                    px-2 sm:px-3 md:px-4 py-1.5 sm:py-2 
+                    ${
+                      isSeries
+                        ? "bg-blue-500 hover:bg-blue-600"
+                        : "bg-red-500 hover:bg-red-600"
+                    }
+                    text-white rounded-lg transition-all duration-200 text-xs sm:text-sm font-medium`}
                 >
-                  <FaUserPlus className="text-sm" />
-                  <span>{t("Register")}</span>
+                  <FaUserPlus className="text-xs sm:text-sm" />
+                  <span className="hidden md:inline">{t("Register")}</span>
                 </Link>
               </div>
             )}
 
             <div
-              className={`flex items-center gap-2 ${
-                isRTL ? "border-r mr-4" : "border-l ml-4"
-              } border-white/10 px-4`}
+              className={`flex items-center gap-1 sm:gap-2 
+              ${isRTL ? "border-r mr-2 sm:mr-4" : "border-l ml-2 sm:ml-4"} 
+              border-white/10 pl-2 sm:pl-4`}
             >
               {[
                 { code: "ar", flag: "EG", label: "Arabic" },
@@ -192,124 +208,7 @@ function NavBar({ isSeries }: { isSeries: boolean }) {
                     handleLanguageChange(code);
                     window.location.href = window.location.pathname;
                   }}
-                  className={`w-8 h-8 rounded-full overflow-hidden transition-all duration-200 ${
-                    locale === code
-                      ? isSeries
-                        ? "ring-2 ring-blue-500 ring-offset-1 ring-offset-black blue-ring"
-                        : "ring-2 ring-red-500 ring-offset-1 ring-offset-black red-ring"
-                      : "opacity-60 hover:opacity-100"
-                  }`}
-                >
-                  <Image
-                    src={`/flags/M/${flag}.svg`}
-                    alt={label}
-                    className="w-full h-full object-cover"
-                    width={32}
-                    height={32}
-                  />
-                </button>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        <div className={`mobile-menu ${isMenuOpen ? "show" : ""}`}>
-          <div className="px-4 py-6 space-y-4">
-            <Link
-              href="/movies"
-              className={`flex items-center gap-2 ${
-                isRTL ? "flex-row-reverse" : ""
-              } px-4 py-3 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-all duration-200 w-full`}
-            >
-              <FaFilm className={isSeries ? "text-blue-500" : "text-red-500"} />
-              <span>{t("Movies")}</span>
-            </Link>
-
-            <Link
-              href="/series"
-              className={`flex items-center gap-2 ${
-                isRTL ? "flex-row-reverse" : ""
-              } px-4 py-3 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-all duration-200 w-full`}
-            >
-              <FaTv className={isSeries ? "text-blue-500" : "text-red-500"} />
-              <span>{t("Series")}</span>
-            </Link>
-
-            {isLogged && (
-              <Link
-                href="/favorites"
-                className={`flex items-center gap-2 ${
-                  isRTL ? "flex-row-reverse" : ""
-                } px-4 py-3 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-all duration-200 w-full`}
-              >
-                <FaHeart
-                  className={isSeries ? "text-blue-500" : "text-red-500"}
-                />
-                <span>{t("Favorites")}</span>
-              </Link>
-            )}
-
-            <div className="space-y-2 pt-4 border-t border-white/10">
-              {isLogged ? (
-                <button
-                  onClick={() => {
-                    localStorage.removeItem("token");
-                    setIsLogged(false);
-                    setIsMenuOpen(false);
-                  }}
-                  className={`flex items-center gap-2 ${
-                    isRTL ? "flex-row-reverse" : ""
-                  } px-4 py-3 ${
-                    isSeries
-                      ? "bg-blue-600 hover:bg-blue-700"
-                      : "bg-red-600 hover:bg-red-700"
-                  } text-white rounded-lg transition-all duration-200 text-sm font-medium w-full`}
-                >
-                  <FaSignOutAlt />
-                  <span>{t("Logout")}</span>
-                </button>
-              ) : (
-                <>
-                  <Link
-                    href="/login"
-                    className={`flex items-center gap-2 ${
-                      isRTL ? "flex-row-reverse" : ""
-                    } px-4 py-3 text-gray-300 hover:text-white hover:bg-white/10 rounded-lg transition-all duration-200 text-sm font-medium w-full`}
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    <FaSignInAlt />
-                    <span>{t("Login")}</span>
-                  </Link>
-                  <Link
-                    href="/register"
-                    className={`flex items-center gap-2 ${
-                      isRTL ? "flex-row-reverse" : ""
-                    } px-4 py-3 ${
-                      isSeries
-                        ? "bg-blue-500 hover:bg-blue-600"
-                        : "bg-red-500 hover:bg-red-600"
-                    } text-white rounded-lg transition-all duration-200 text-sm font-medium w-full`}
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    <FaUserPlus />
-                    <span>{t("Register")}</span>
-                  </Link>
-                </>
-              )}
-            </div>
-
-            <div className="flex justify-center gap-4 pt-4 border-t border-white/10">
-              {[
-                { code: "ar", flag: "EG", label: "Arabic" },
-                { code: "en", flag: "840", label: "English" },
-              ].map(({ code, flag, label }) => (
-                <button
-                  key={code}
-                  onClick={() => {
-                    handleLanguageChange(code);
-                    window.location.href = window.location.pathname;
-                  }}
-                  className={`w-8 h-8 rounded-full overflow-hidden transition-all duration-200 ${
+                  className={`w-6 h-6 sm:w-8 sm:h-8 rounded-full overflow-hidden transition-all duration-200 ${
                     locale === code
                       ? isSeries
                         ? "ring-2 ring-blue-500 ring-offset-1 ring-offset-black blue-ring"
